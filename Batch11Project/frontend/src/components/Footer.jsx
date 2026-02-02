64335d36e2ccdd998ffdc4edff0a92bc8bbcd113
521501 <swornajyoti510@gmail.com>
2026-02-02 20:19:19 +0545

    Delete Batch11Project directory

--- Batch11Project/frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  const currentYear = new Date().getFullYear();
-  return (
-    <footer>
-      <div className="my-3 text-center">&copy; Broadway, {currentYear}</div>
-    </footer>
-  );
-}
-export default Footer;
--- Batch11Project/frontend/src/constants.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = "";
-export const PRODUCT_URL = "/api/products";
-export const USER_URL = "/api/user";
-export const ORDER_URL = "/api/orders";
